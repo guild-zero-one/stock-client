10e65b1145b3a0d912edc83a3a2ca4e731365558
joaoGlima09 <joao.glima@sptech.school>
2025-04-27 23:49:33 -0300

    refactor: :recycle: refatorando com prettier

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
 export default function NotFound() {
-    return (
-      <div className="flex flex-col justify-center items-center h-screen text-text-default text-center">
-        <h1 className="mb-4 font-bold text-4xl">404 - Página não encontrada</h1>
-        <p className="text-text-secundary">Desculpe, não conseguimos encontrar o que você procura.</p>
-        <Link href={'/'}><span className="text-pink-default text-xs">Ir para Página Principal</span></Link>
-      </div>
-    );
-  }
+  return (
+    <div className="flex flex-col justify-center items-center h-screen text-text-default text-center">
+      <h1 className="mb-4 font-bold text-4xl">404 - Página não encontrada</h1>
+      <p className="text-text-secundary">Desculpe, não conseguimos encontrar o que você procura.</p>
+      <Link href={"/"}>
+        <span className="text-pink-default text-xs">Ir para Página Principal</span>
+      </Link>
+    </div>
+  );
+}
