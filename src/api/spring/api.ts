4cedcfb3c6aeb77b1b79791d22c0b670fa252c37
Fernando <fernando.soares@sptech.school>
2025-05-01 13:54:53 -0300

    :sparkles: feat: ajustando token

--- src/api/spring/api.ts
+++ src/api/spring/api.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:8080",
+  withCredentials: true,
   timeout: 10000, //10 Segundos
   headers: {
     "Content-Type": "application/json",
--- src/api/spring/services/UsuarioService.ts
+++ src/api/spring/services/UsuarioService.ts
@@ -5,13 +5,16 @@ const router = "/usuarios";
 
 export const login = async (user: UsuarioLogin) => {
   try {
-    const response = await api.post(`${router}/login`, user);
+    const response = await api.post(`${router}/login`, user, {
+      withCredentials: true
+    });
     return response.data;
   } catch (error) {
     console.error("Erro ao realizar o login:", error);
     throw error;
   }
 };
+
 
 export const usuarioPorId = async (id: number) => {
   try {
