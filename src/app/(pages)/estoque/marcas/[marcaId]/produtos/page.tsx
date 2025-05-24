ea7873ca6ea6188e05a24797dc94a9099825bd11
Fernando <fernando.soares@sptech.school>
2025-05-23 23:25:20 -0300

    ✨ feat: adicionando filtragem por nome na listagem de produtos por marca

--- src/app/(pages)/estoque/marcas/[marcaId]/produtos/page.tsx
+++ src/app/(pages)/estoque/marcas/[marcaId]/produtos/page.tsx
@@ -25,6 +25,12 @@ export default function ProdutosPage() {
   const { marcaId } = useParams();
   const [fornecedor, setFornecedor] = useState<Fornecedor>();
   const [produtos, setProdutos] = useState<Produto[]>([]);
+  const [inputPesquisar, setInputPesquisar] = useState("");
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setInputPesquisar(value);
+  };
 
   useEffect(() => {
     const fetchFornecedor = async () => {
@@ -34,13 +40,17 @@ export default function ProdutosPage() {
 
         setProdutos(produtos);
         setFornecedor(fornecedor);
-        console.log("Fornecedor: ", fornecedor.nome)
-        console.log("Produtos: ", produtos)
       }
     };
     fetchFornecedor();
   }, []);
 
+  const produtosFiltrados = produtos.filter((produto) => produto
+    .nome
+    .toLowerCase()
+    .includes(inputPesquisar
+      .toLowerCase()
+      .trim()));
 
   return (
     <div className="flex flex-col w-full min-h-dvh">
@@ -53,14 +63,28 @@ export default function ProdutosPage() {
 
       {/* Grid */}
       <div className="flex flex-col gap-4 p-4 w-full">
-        <Input name="search" label="Pesquisar" showIcon iconSymbol={<SearchIcon />} inputSize="small" />
+        <Input name="search" label="Pesquisar" iconSymbol={<SearchIcon />} handleChange={handleSearchChange} />
       </div>
 
-      {fornecedor && (
+      {inputPesquisar.length === 0 ? (
+
         <div className="gap-4 grid grid-cols-1 px-4">
-          <ProductsList produtos={produtos} fornecedor={fornecedor} />
+          <ProductsList produtos={produtos} fornecedor={fornecedor!} />
+        </div>
+
+      ) : produtosFiltrados.length > 0 ? (
+        <div className="gap-4 grid grid-cols-1 px-4">
+          <ProductsList produtos={produtosFiltrados} fornecedor={fornecedor!} />
+        </div>
+
+      ) : (
+        <div className="flex justify-center items-center py-4 font-medium text-pink-secondary-dark">
+          <h2 className="italic">Nenhum produto encontrado</h2>
         </div>
       )}
+
+
+
     </div>
   );
 }
