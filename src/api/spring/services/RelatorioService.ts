5fd443ae24e4437d6997326ff19dca2c4e56c323
joaoGlima09 <joao.glima@sptech.school>
2025-06-10 19:09:34 -0300

    refactor: :recycle: Ajustando relatório service com novas rotas

--- src/api/spring/services/RelatorioService.ts
+++ src/api/spring/services/RelatorioService.ts
@@ -2,6 +2,18 @@ import api from "../api";
 
 const router = "/relatorios";
 
+export const quantidadePedidosEmAberto = async () => {
+  try {
+    const response = await api.get<number>(
+      `${router}/quantidade-pedidos-em-aberto`
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Erro ao listar quantidade pedidos em aberto:", error);
+    throw error;
+  }
+};
+
 export const totalVendasMensal = async () => {
   try {
     const response = await api.get<number>(`${router}/total-vendas-mensal`);
@@ -12,21 +24,9 @@ export const totalVendasMensal = async () => {
   }
 };
 
-export const valoresVendasUltimos6Meses = async () => {
-  try {
-    const response = await api.get<number[]>(
-      `${router}/valores-vendas-ultimos-6-meses`
-    );
-    return response.data;
-  } catch (error) {
-    console.error("Erro ao listar valores vendas últimos 6 meses:", error);
-    throw error;
-  }
-};
-
 export const top3ProdutosMes = async () => {
   try {
-    const response = await api.get<number[]>(`${router}/top3-produtos-mes`);
+    const response = await api.get<string[]>(`${router}/top3-produtos-mes`);
     return response.data;
   } catch (error) {
     console.error("Erro ao listar top 3 produtos:", error);
@@ -34,10 +34,10 @@ export const top3ProdutosMes = async () => {
   }
 };
 
-export const quantidadeProdutosUltimos6Meses = async () => {
+export const faturamentoUltimos6Meses = async () => {
   try {
-    const response = await api.get<number[]>(
-      `${router}/quantidade-produtos-ultimos-6-meses`
+    const response = await api.get<Record<string, number>>(
+      `${router}/faturamento-ultimos-6-meses`
     );
     return response.data;
   } catch (error) {
@@ -46,14 +46,14 @@ export const quantidadeProdutosUltimos6Meses = async () => {
   }
 };
 
-export const quantidadePedidosEmAberto = async () => {
+export const quantidadePedidosUltimos6Meses = async () => {
   try {
-    const response = await api.get<number>(
-      `${router}/quantidade-pedidos-em-aberto`
+    const response = await api.get<Record<string, number>>(
+      `${router}/quantidade-pedidos-ultimos-6-meses`
     );
     return response.data;
   } catch (error) {
-    console.error("Erro ao listar quantidade pedidos em aberto:", error);
+    console.error("Erro ao listar valores vendas últimos 6 meses:", error);
     throw error;
   }
 };
