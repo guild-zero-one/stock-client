db37bc562be0acf6b3615f55098f689ccd1503c5
joaoGlima09 <joao.glima@sptech.school>
2025-06-11 04:21:40 -0300

    feat: :sparkles: Fluxo de produto integrado

--- src/app/(pages)/estoque/marcas/[marcaId]/editar/page.tsx
+++ src/app/(pages)/estoque/marcas/[marcaId]/editar/page.tsx
@@ -1,122 +1,123 @@
-'use client'
+"use client";
 import { marcaPorId } from "@/api/spring/services/FornecedorService";
 import Button from "@/components/button";
 import ButtonFile from "@/components/button-file";
 import Header from "@/components/header";
 import Input from "@/components/input";
 import { Fornecedor } from "@/models/Fornecedor/Fornecedor";
-import ImageIcon from '@mui/icons-material/Image';
+import ImageIcon from "@mui/icons-material/Image";
 import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function CriarMarca() {
-    const { marcaId } = useParams();
-    const [marca, setMarca] = useState<Fornecedor>();
-    const [marcaEditada, setMarcaEditada] = useState<Fornecedor>();
-    const [editar, setEditar] = useState(false);
+  const { marcaId } = useParams();
 
-    const inative = () => {
-        return editar ? false : true;
+  const [marca, setMarca] = useState<Fornecedor>();
+  const [marcaEditada, setMarcaEditada] = useState<Fornecedor>();
+  const [editar, setEditar] = useState(false);
+
+  const inative = () => {
+    return editar ? false : true;
+  };
+  const handleCancelar = () => {
+    setMarcaEditada(marca);
+    setEditar(false);
+  };
+
+  useEffect(() => {
+    atualizarMarca();
+  }, []);
+
+  const atualizarMarca = async () => {
+    const marca = await marcaPorId(Number(marcaId));
+    if (marca) {
+      setMarca(marca);
+      setMarcaEditada({ ...marca });
     }
-    const handleCancelar = () => {
-        setMarcaEditada(marca);
-        setEditar(false);
+  };
+
+  const handleMarcaEditada =
+    (field: keyof Fornecedor) => (e: React.ChangeEvent<HTMLInputElement>) => {
+      setMarcaEditada((prev) =>
+        prev ? { ...prev, [field]: e.target.value } : prev
+      );
     };
 
-    useEffect(() => {
-        atualizarMarca()
-        console.log(marcaId)
-    }, []);
+  return (
+    <main className="relative flex flex-col w-full min-h-screen">
+      <Header title="Editar" subtitle="Marca" />
 
-    const atualizarMarca = async () => {
-        const marca = await marcaPorId(Number(marcaId));
-        if (marca) {
-            setMarca(marca);
-            setMarcaEditada({ ...marca });
-        }
-    }
+      {/* Grid */}
+      <div className="flex flex-col gap-4 p-4 w-full">
+        <div className="flex justify-between">
+          <div className="flex justify-center items-center border border-gray-dark rounded w-36 h-36 text-gray-300 text-6xl">
+            {marca ? (
+              <div>
+                <img src={marca.imagemUrl} alt="" />
+              </div>
+            ) : (
+              <ImageIcon fontSize="inherit" />
+            )}
+          </div>
 
+          <ButtonFile
+            id="imagem-marca"
+            onSelect={(file) => console.log(file)}
+            label="Adicionar Imagem"
+            accept="image/*"
+          />
+        </div>
 
-    const handleMarcaEditada = (field: keyof Fornecedor) => (e: React.ChangeEvent<HTMLInputElement>) => {
-        setMarcaEditada((prev) =>
-            prev ? { ...prev, [field]: e.target.value } : prev
-        );
-    };
+        {/* Inputs */}
+        <div className="flex flex-col gap-2">
+          <Input
+            label="Nome"
+            name="marca-name"
+            disabled={inative()}
+            value={editar ? marcaEditada?.nome ?? "" : marca?.nome ?? ""}
+            handleChange={handleMarcaEditada("nome")}
+          />
+          <Input
+            label="Descrição"
+            name="marca-description"
+            disabled={inative()}
+            value={
+              editar ? marcaEditada?.descricao ?? "" : marca?.descricao ?? ""
+            }
+            handleChange={handleMarcaEditada("descricao")}
+          />
+          <Input
+            label="CNPJ"
+            name="marca-cnpj"
+            disabled={inative()}
+            value={editar ? marcaEditada?.cnpj ?? "" : marca?.cnpj ?? ""}
+            handleChange={handleMarcaEditada("cnpj")}
+          />
+        </div>
 
-    return (
-        <main className="relative flex flex-col w-full min-h-screen">
-            <Header title="Editar" subtitle="Marca" />
-
-            {/* Grid */}
-            <div className="flex flex-col gap-4 p-4 w-full">
-                <div className="flex justify-between">
-                    <div className="flex justify-center items-center border border-gray-dark rounded w-36 h-36 text-gray-300 text-6xl">
-                        {marca ? (
-                            <div>
-                                <img src={marca.imagemUrl} alt="" />
-                            </div>
-                        ) : (
-                            <ImageIcon fontSize="inherit" />
-                        )}
-                    </div>
-
-                    <ButtonFile onSelect={(file) => console.log(file)} label="Adicionar Imagem" accept="image/*" />
-                </div>
-
-                {/* Inputs */}
-                <div className="flex flex-col gap-2">
-                    <Input
-                        label="Nome"
-                        name="marca-name"
-                        disabled={inative()}
-                        value={
-                            editar
-                                ? marcaEditada?.nome ?? ""
-                                : marca?.nome ?? ""
-                        }
-                        handleChange={handleMarcaEditada("nome")}
-                    />
-                    <Input
-                        label="Descrição"
-                        name="marca-description"
-                        disabled={inative()}
-                        value={
-                            editar
-                                ? marcaEditada?.descricao ?? ""
-                                : marca?.descricao ?? ""
-                        }
-                        handleChange={handleMarcaEditada("descricao")}
-                    />
-                    <Input
-                        label="CNPJ"
-                        name="marca-cnpj"
-                        disabled={inative()}
-                        value={
-                            editar
-                                ? marcaEditada?.cnpj ?? ""
-                                : marca?.cnpj ?? ""
-                        }
-                        handleChange={handleMarcaEditada("cnpj")}
-                    />
-                </div>
-
-                {/* Buttons */}
-                {editar ? (
-                    <div className="flex flex-col gap-2">
-                        <Button label="Confirmar alterações" fullWidth />
-                        <Button label="Cancelar" fullWidth variant="outlined" onClick={handleCancelar} />
-                    </div>
-                ) : (
-                    <>
-                        <div className="flex flex-col gap-2">
-                            <Button label="Editar Marca" fullWidth onClick={() => setEditar(!editar)} />
-                        </div>
-                    </>
-                )}
-
-
+        {/* Buttons */}
+        {editar ? (
+          <div className="flex flex-col gap-2">
+            <Button label="Confirmar alterações" fullWidth />
+            <Button
+              label="Cancelar"
+              fullWidth
+              variant="outlined"
+              onClick={handleCancelar}
+            />
+          </div>
+        ) : (
+          <>
+            <div className="flex flex-col gap-2">
+              <Button
+                label="Editar Marca"
+                fullWidth
+                onClick={() => setEditar(!editar)}
+              />
             </div>
-
-        </main>
-    )
+          </>
+        )}
+      </div>
+    </main>
+  );
 }
--- src/app/(pages)/estoque/marcas/[marcaId]/produtos/criar/adicionarProdutos.tsx
+++ src/app/(pages)/estoque/marcas/[marcaId]/produtos/criar/adicionarProdutos.tsx
@@ -1,63 +1,272 @@
+import { useEffect, useState, useMemo } from "react";
+import { useRouter } from "next/navigation";
+
 import { marcaPorId } from "@/api/spring/services/FornecedorService";
-import AccordionProductList from "@/components/accordion/accordion-item";
-import Accordion from "@/components/accordion/products-accordion";
-import FooterProductList from "@/components/footer-product-list";
+import {
+  cadastrarProduto,
+  produtoPorSku,
+  atualizarProduto,
+} from "@/api/spring/services/ProdutoService";
+import { cadastrarImagem } from "@/api/spring/services/ImagenService";
+
 import { Fornecedor } from "@/models/Fornecedor/Fornecedor";
 import { ProdutoCreate } from "@/models/Produto/Produto";
-import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
+
+import AccordionProductList from "@/components/accordion/accordion-item";
+import Accordion from "@/components/accordion/products-accordion";
+
+import FooterProductList from "@/components/footer-product-list";
+import Button from "@/components/button";
+import Modal from "@/components/modal-popup";
+
+import {
+  AssignmentLateOutlined,
+  AssignmentTurnedInOutlined,
+} from "@mui/icons-material";
+import Toast from "@/components/toast";
 
 interface ResumoProps {
-    produtos: ProdutoCreate[];
-    showProdList: boolean;
-    setShowProdList: (value: boolean) => void;
+  produtos: ProdutoCreate[];
+  showProdList: boolean;
+  setShowProdList: (value: boolean) => void;
 }
-export default function ResumoProdutos({ produtos, showProdList, setShowProdList } : ResumoProps){
 
-    const [marcas, setMarcas] = useState<Fornecedor[]>([]);
+export default function ResumoProdutos({
+  produtos,
+  showProdList,
+  setShowProdList,
+}: ResumoProps) {
+  const [produtosResumo, setProdutosResumo] =
+    useState<ProdutoCreate[]>(produtos);
+  const [marcas, setMarcas] = useState<Fornecedor[]>([]);
 
-    useEffect(() => {
+  const [modalAberto, setModalAberto] = useState(false);
+  const [modalTipo, setModalTipo] = useState<"cancelar" | "cadastrar" | null>(
+    null
+  );
 
-        const idsUnicos = Array.from(new Set(produtos.map(p => p.fornecedorId)));
+  const [toast, setToast] = useState<"success" | "error" | null>(null);
 
-        const fetchFornecedores = async () => {
-            try {
-                const respostas = await Promise.all(
-                    idsUnicos.map(id => marcaPorId(id))
-                );
-                const fornecedoresValidos = respostas.filter(Boolean);
-                setMarcas(fornecedoresValidos);
-            } catch (erro) {
-                console.error("Erro ao buscar fornecedores:", erro);
-            }
-        };
+  const showToast = (type: "success" | "error" | null) => {
+    setToast(null);
+    setTimeout(() => {
+      setToast(type);
+    }, 10);
+  };
 
-        fetchFornecedores();
-    }, [produtos]);
+  const toastMap = {
+    success: {
+      title: "Produtos cadastrados com sucesso",
+      message: "Você será redirecionado para o estoque",
+      type: "success",
+    },
+    error: {
+      title: "Erro ao cadastrar produtos",
+      message: "Verique as informações e tente novamente.",
+      type: "error",
+    },
+  } as const;
 
-    return (
+  useEffect(() => {
+    const idsUnicos = Array.from(new Set(produtos.map((p) => p.fornecedorId)));
 
-        <div>
-            {/* Grid */}
-            <div className="flex flex-col gap-4 p-4 w-full">
-                <ul>
-                    {marcas.map(marca => {
-                        const produtosDaMarca = produtos.filter(produto => produto.fornecedorId === marca.id);
+    const fetchFornecedores = async () => {
+      try {
+        const respostas = await Promise.all(
+          idsUnicos.map((id) => marcaPorId(id))
+        );
+        const fornecedoresValidos = respostas.filter(Boolean);
+        setMarcas(fornecedoresValidos);
+      } catch (erro) {
+        console.error("Erro ao buscar fornecedores:", erro);
+      }
+    };
 
-                        return (
-                            <li key={marca.id}>
-                                <Accordion title={marca.nome} defaultOpen={true} badgeValue={produtosDaMarca.length}>
-                                    <AccordionProductList produtos={produtosDaMarca} />
-                                </Accordion>
-                            </li>
-                        );
-                    })}
-                </ul>
+    fetchFornecedores();
+  }, [produtos]);
 
-            </div>
+  const router = useRouter();
 
-            <FooterProductList total={0} onAdd={() => setShowProdList(false)} />
+  const cadastrarProdutos = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        </div>
+    try {
+      setModalAberto(false);
+
+      for (const produto of produtosResumo) {
+        const produtoExistente = await produtoPorSku(produto.sku);
+
+        if (produtoExistente && produtoExistente.id) {
+          const quantidadeAtualizada =
+            produtoExistente.quantidade + produto.quantidade;
+          produtoExistente.quantidade = quantidadeAtualizada;
+
+          await atualizarProduto(produtoExistente.id, produtoExistente);
+        } else {
+          const produtoCriado = await cadastrarProduto(produto);
+
+          if (produto.imagem) {
+            await cadastrarImagem(produtoCriado.id, produto.imagem);
+          }
+        }
+      }
+
+      showToast("success");
+
+      setTimeout(() => {
+        router.push("/estoque/marcas");
+      }, 3000);
+    } catch (error) {
+      console.error("Erro ao cadastrar produtos:", error);
+      showToast("error");
+    }
+  };
+
+  const cancelarPedido = () => {
+    router.push("/estoque/marcas");
+  };
+
+  const totalGeral = useMemo(
+    () =>
+      produtosResumo.reduce(
+        (acc, prod) =>
+          acc +
+          Number(prod.quantidade) *
+            Number(prod.valorVenda ?? prod.precoUnitario ?? 0),
+        0
+      ),
+    [produtos]
+  );
+
+  const handleChangeQuantidade = (index: number, novaQuantidade: number) => {
+    setProdutosResumo((prev) =>
+      prev.map((prod, i) =>
+        i === index ? { ...prod, quantidade: novaQuantidade } : prod
+      )
     );
+  };
+
+  const handleDeleteProduto = (sku: string) => {
+    setProdutosResumo((prev) => prev.filter((prod) => prod.sku !== sku));
+  };
+
+  return (
+    <div>
+      {toast && <Toast {...toastMap[toast]} />}
+
+      {/* Grid */}
+      <div className="flex flex-col gap-4 p-4 w-full pb-18">
+        <ul>
+          {marcas.map((marca) => {
+            const produtosDaMarca = produtosResumo
+              .map((produto, i) => ({ produto, i }))
+              .filter(({ produto }) => produto.fornecedorId === marca.id);
+
+            return (
+              <>
+                <li key={marca.id}>
+                  <Accordion
+                    title={marca.nome}
+                    defaultOpen={true}
+                    badgeValue={produtosDaMarca.length}
+                  >
+                    <AccordionProductList
+                      produtos={produtosDaMarca.map(({ produto }) => produto)}
+                      onChangeQuantidade={(index, novaQuantidade) => {
+                        const idxReal = produtosDaMarca[index].i;
+                        handleChangeQuantidade(idxReal, novaQuantidade);
+                      }}
+                      onDeleteProduto={(sku) => handleDeleteProduto(sku)}
+                    />
+                  </Accordion>
+                </li>
+
+                <Modal
+                  open={modalAberto}
+                  icon={
+                    modalTipo === "cancelar" ? (
+                      <AssignmentLateOutlined fontSize="inherit" />
+                    ) : (
+                      <AssignmentTurnedInOutlined fontSize="inherit" />
+                    )
+                  }
+                  onClose={() => {
+                    setModalAberto(false);
+                  }}
+                  title={
+                    modalTipo === "cancelar" ? (
+                      <p>Cancelar cadastro de produtos</p>
+                    ) : (
+                      <p>Cadastrar produtos</p>
+                    )
+                  }
+                  body={
+                    modalTipo === "cancelar" ? (
+                      <div className="flex justify-center flex-col gap-4">
+                        <span className="flex flex-col gap-1">
+                          <p className="w-full text-center">
+                            Deseja cancelar o cadastramento deste lote?
+                          </p>
+                          <p className="text-pink-default font-bold text-center">
+                            Essa ação é irreversível e não poderá ser desfeita.
+                          </p>
+                        </span>
+
+                        <span className="flex flex-col gap-2">
+                          <Button
+                            onClick={() => setModalAberto(false)}
+                            fullWidth
+                            label="Não, não desejo cancelar"
+                            variant="outlined"
+                          ></Button>
+                          <Button
+                            onClick={cancelarPedido}
+                            fullWidth
+                            label="Sim, desejo cancelar"
+                          ></Button>
+                        </span>
+                      </div>
+                    ) : (
+                      <form
+                        onSubmit={cadastrarProdutos}
+                        className="flex justify-center flex-col gap-2"
+                      >
+                        <p className="w-full text-center">
+                          Deseja cadastrar lote de produtos?
+                        </p>
+
+                        <Button
+                          fullWidth
+                          variant="outlined"
+                          label="Não, desejo adicionar mais produtos"
+                          onClick={() => setModalAberto(false)}
+                        ></Button>
+                        <Button
+                          type="submit"
+                          fullWidth
+                          label="Sim, desejo Cadastrar"
+                        ></Button>
+                      </form>
+                    )
+                  }
+                />
+              </>
+            );
+          })}
+        </ul>
+      </div>
+
+      {/* Agora o total vai certinho pro footer */}
+      <FooterProductList
+        total={totalGeral}
+        onAdd={() => setShowProdList(false)}
+        onConfirm={() => {
+          setModalAberto(true), setModalTipo("cadastrar");
+        }}
+        onCancel={() => {
+          setModalAberto(true), setModalTipo("cancelar");
+        }}
+      />
+    </div>
+  );
 }
