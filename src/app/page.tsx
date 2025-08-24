3e7085a7671e5ba3bbcd51591631c1f78491a7ff
joaoGlima09 <joao.glima@sptech.school>
2025-08-24 00:52:12 -0300

    style: :art: arrumando as imagens nas páginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 import Link from "next/link";
 import Image from "next/image";
+import peopleImage from "@/assets/images/people.jpg";
 
 import { usuarioAutenticado } from "@/api/spring/services/UsuarioService";
 import { quantidadePedidosEmAberto } from "@/api/spring/services/RelatorioService";
@@ -48,13 +49,7 @@ export default function Home() {
       {/* Banner */}
       <header className="relative h-[45vh] text-white">
         {/* Imagem de fundo */}
-        <Image
-          className="z-0 object-center object-cover"
-          src="/assets/images/people.jpg"
-          alt="Pessoas"
-          fill
-          priority
-        />
+        <Image className="z-0 object-center object-cover" src={peopleImage} alt="Pessoas" fill priority />
 
         {/* Filtro de cor */}
         <div className="absolute inset-0 bg-pink-default/65" />
@@ -64,10 +59,7 @@ export default function Home() {
           <h1 className="font-bold text-2xl">Boas vindas {usuario?.nome}</h1>
           <Link href={"/usuario"}>
             <div className="group flex justify-center items-center bg-pink-default rounded-full w-8 h-8 text-gray-default text-2xl">
-              <AccountCircleIcon
-                className="group-hover:scale-125"
-                fontSize="inherit"
-              />
+              <AccountCircleIcon className="group-hover:scale-125" fontSize="inherit" />
             </div>
           </Link>
         </div>
@@ -79,12 +71,8 @@ export default function Home() {
         <div className="gap-2 grid grid-cols-2 w-full h-fit">
           {/* Dashboard View */}
           <div className="flex flex-col justify-center items-start col-span-2 bg-gray-default p-4 rounded-xl w-full h-[10vh] min-h-[120px] max-h-[300px]">
-            <span className="text-text-secondary text-sm">
-              Total de pedidos abertos
-            </span>
-            <span className="font-bold text-text-default text-3xl">
-              {kpiTotalPedidos}
-            </span>
+            <span className="text-text-secondary text-sm">Total de pedidos abertos</span>
+            <span className="font-bold text-text-default text-3xl">{kpiTotalPedidos}</span>
           </div>
 
           {/* Clientes */}
